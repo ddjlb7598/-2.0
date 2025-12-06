16b37288ee2bdd3bc71c9684fb243ed72ac04c41
ddjlb7598 <q18342117598@163.com>
2025-12-07 02:52:09 +0800

    Update 迪脚本.lua

--- 迪脚本.lua
+++ 迪脚本.lua
@@ -1262,9 +1262,8 @@ GeneralSection:Button("迪自瞄", function()
   
   loadstring(game:HttpGet([[https://raw.githubusercontent.com/xiaopi77/xiaopi77/main/3683e49998644fb7.txt_2024-08-09_094310.OTed.lua]]))()
 end)
-GeneralSection:Button("龙脚本迪脚本作者", function()
-
-
+GeneralSection:Button("龙脚本12.7", function()
+  
   loadstring(game:HttpGet("https://raw.githubusercontent.com/nahida-cn/Roblox/main/long"))()
 end)
 GeneralSection:Button("甩飞所有人", function()
@@ -6873,13 +6872,6 @@ r50_0:Tab("『汽车经销大亨』", "18930406865"):section("整合脚本", tru
   loadstring(game:HttpGet([[https://raw.githubusercontent.com/xiaopi77/Integration-script/refs/heads/main/Roblox-Pi-Script-Automobiledealertycoon.lua]]))()
 
 end)    
-
-
-
-
-
-
-
 
 
 
